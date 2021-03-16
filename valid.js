8c95ac0fb305818b57d152549c8ac42632420367
deen26 <aaxam40@gmail.com>
2021-03-16 10:27:48 +0300

    event listeners

--- valid.js
+++ valid.js
@@ -7,3 +7,6 @@ const username_error = document.getElementById('username_error');
 const email_error = document.getElementById('email_error');
 const pass_error = document.getElementById('pass_error');
 const confirm_pass_error = document.getElementById('confirm_pass_error');
+
+email.addEventListener('textInput', email_Verify);
+password.addEventListener('textInput', pass_Verify);
