d53b438210d5a00dc1c36aaa7ab288f12936007b
deen26 <aaxam40@gmail.com>
2021-03-16 10:27:04 +0300

    errors

--- valid.js
+++ valid.js
@@ -1,4 +1,9 @@
-const email = document.forms['form']['usrname'];
+const email = document.forms['form']['username'];
 const password = document.forms['form']['email'];
 const email = document.forms['form']['pasword'];
 const password = document.forms['form']['confirm_password'];
+
+const username_error = document.getElementById('username_error');
+const email_error = document.getElementById('email_error');
+const pass_error = document.getElementById('pass_error');
+const confirm_pass_error = document.getElementById('confirm_pass_error');
