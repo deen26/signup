af6a3c4626ad03ef76c556726b68172d4c026814
deen26 <aaxam40@gmail.com>
2021-03-16 10:22:01 +0300

    created constants

---
+++ valid.js
@@ -0,0 +1,4 @@
+const email = document.forms['form']['usrname'];
+const password = document.forms['form']['email'];
+const email = document.forms['form']['pasword'];
+const password = document.forms['form']['confirm_password'];
